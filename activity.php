99b5bc104239bbd0a30dfd4ce6860f9e45e51254
CrackedNamless <55303710+FendinX@users.noreply.github.com>
2024-02-05 14:27:13 +0100

    activity.php

--- activity.php
+++ activity.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'account-activity-log' => 'Account Activity Log',
-    'activity-log' => 'Activity Log',
-    'clear-filters' => 'Clear Filters',
-    'metadata' => 'Metadata',
-    'close' => 'Close',
-    'no-logs-server' => 'No activity logs available for this server.'
+    'account-activity-log' => 'Registro Attività dell\'Account',
+    'activity-log' => 'Registro Attività',
+    'clear-filters' => 'Cancella Filtri',
+    'metadata' => 'Metadati',
+    'close' => 'Chiudi',
+    'no-logs-server' => 'Nessun registro di attività disponibile per questo server.'
 ];
